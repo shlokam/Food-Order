ab4073aa510151f2ab374537d2c92660ee9b03c7
Shloka Mahesheka <shloka.mahesheka012@gmail.com>
2022-12-16 14:47:49 +0530

    Made navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React,{useState} from "react";
 import logo from '../assests/logo.jpeg';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css'
+import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar(){
+
+    const [openLinks, setOpenLinks] = useState(false);
+
+    const toggleNavbar = () => {
+        setOpenLinks(!openLinks);
+      };
+
     return(
         <div className="navbar">
-            <div className="leftSide">
+            <div className="leftSide" id = {openLinks ? "open" : "close"}>
                 <img src={logo} />
+                <div className="hiddenLinks">
+                <Link to= "/">Home</Link>
+                <Link to= "/">Menu</Link>
+                <Link to= "/">About Us</Link>
+                <Link to= "/">Contact</Link>
+                </div>
             </div>
             <div className="rightSide">
                 <Link to= "/">Home</Link>
                 <Link to= "/">Menu</Link>
                 <Link to= "/">About Us</Link>
                 <Link to= "/">Contact</Link>
+                <button onClick={toggleNavbar}>
+                    <ReorderIcon/>
+                </button>
             </div>
 
         </div>
