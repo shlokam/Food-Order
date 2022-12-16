d8c2dce0e2d942ae1009e718beece8b9ef094d21
Shloka Mahesheka <shloka.mahesheka012@gmail.com>
2022-12-16 15:28:44 +0530

    Added home page.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
-
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar/>
+        <Routes>
+          <Route path="/" element = {<Home/>}/>
+        </Routes>
       </Router>
     </div>
     
